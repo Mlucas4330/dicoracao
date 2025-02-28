73e06d9936a5a1778f0a8bb67458800e28f2d756
mlucas4330 <mlucas4330@gmail.com>
2025-02-28 01:58:36 -0300

    remocao de slide

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,9 +6,9 @@ function Hero() {
     return (
         <>
             <Box my={16} textAlign={'center'} gap={4} justifyItems={'center'}>
-                <Heading pb={2} size={'5xl'} fontWeight={'bold'}>Transforme seu amor em surpresa</Heading>
+                <Heading pb={2} size={'6xl'} fontWeight={'bold'}>Transforme seu amor em surpresa</Heading>
                 <Text w={{ md: '1/2' }} pb={6}>Crie uma experiência inesquecível! Personalize uma surpresa especial que será revelada através de um QR Code único.</Text>
-                <Button as={'a'} href={'#create'} size={'lg'}>Quero criar minha surpresa</Button>
+                <Button as={'a'} href={'#create'} size={'2xl'}>Quero criar minha surpresa</Button>
             </Box>
         </>
     )
